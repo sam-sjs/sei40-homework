9fc3a271db33639d7df76cb0874d56c01c8cb79b
Sam Skinner <sam.sjskinner@gmail.com>
2020-12-16 14:07:08 +1100

    bug fixes

--- sam_skinner/week7/mars-rover/js/main.js
+++ sam_skinner/week7/mars-rover/js/main.js
@@ -11,10 +11,11 @@ $(document).ready(function() {
     $.getJSON(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=9igxpiYwTamUPFnOPvQdRhsrpwcH8bEEm79MgD6z`)
     .done(function(response) {
       const photos = response.photos
-      $('#gridContainer').css({'gridTemplateRows': 'repeat(photos.length, 1fr)',
-                               'gridTemplateColumns': 'repeat(photos.length, 1fr)'});
+      const countPhotos = photos.length > 25 ? 25 : photos.length
+      $('#gridContainer').css({gridTemplateRows: `repeat(${countPhotos / 5}, 1fr)`,
+                               gridTemplateColumns: `repeat(${countPhotos / 5}, 1fr)`});
 
-      for(let i = 0; i < photos.length; i++) {
+      for(let i = 0; i < countPhotos; i++) {
         const $photo = $(`<div><img src="${photos[i].img_src}"></div>`);
         $('#gridContainer').append($photo);
       }; // for
